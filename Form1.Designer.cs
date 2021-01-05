fb3b4a5164f4e9188ad42c00cfafb85a8db110d5
Kiss Bence <kiss.bence@verebelyszki.hu>
2021-01-05 19:55:28 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WFA210104
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpStart = new System.Windows.Forms.DateTimePicker();
+            this.dtpEnd = new System.Windows.Forms.DateTimePicker();
+            this.lblStart = new System.Windows.Forms.Label();
+            this.lblEnd = new System.Windows.Forms.Label();
+            this.lblText = new System.Windows.Forms.Label();
+            this.lblValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dtpStart
+            // 
+            this.dtpStart.Location = new System.Drawing.Point(114, 12);
+            this.dtpStart.Name = "dtpStart";
+            this.dtpStart.Size = new System.Drawing.Size(200, 20);
+            this.dtpStart.TabIndex = 0;
+            this.dtpStart.Value = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            this.dtpStart.ValueChanged += new System.EventHandler(this.Dtp_ValueChanged);
+            // 
+            // dtpEnd
+            // 
+            this.dtpEnd.Location = new System.Drawing.Point(114, 37);
+            this.dtpEnd.Name = "dtpEnd";
+            this.dtpEnd.Size = new System.Drawing.Size(200, 20);
+            this.dtpEnd.TabIndex = 1;
+            this.dtpEnd.Value = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            this.dtpEnd.ValueChanged += new System.EventHandler(this.Dtp_ValueChanged);
+            // 
+            // lblStart
+            // 
+            this.lblStart.AutoSize = true;
+            this.lblStart.Location = new System.Drawing.Point(8, 18);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(69, 13);
+            this.lblStart.TabIndex = 2;
+            this.lblStart.Text = "Kezdő dátum";
+            // 
+            // lblEnd
+            // 
+            this.lblEnd.AutoSize = true;
+            this.lblEnd.Location = new System.Drawing.Point(8, 43);
+            this.lblEnd.Name = "lblEnd";
+            this.lblEnd.Size = new System.Drawing.Size(64, 13);
+            this.lblEnd.TabIndex = 2;
+            this.lblEnd.Text = "Vége dátum";
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Location = new System.Drawing.Point(8, 73);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(60, 13);
+            this.lblText.TabIndex = 3;
+            this.lblText.Text = "Különbség:";
+            // 
+            // lblValue
+            // 
+            this.lblValue.AutoSize = true;
+            this.lblValue.Location = new System.Drawing.Point(8, 89);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.Size = new System.Drawing.Size(0, 13);
+            this.lblValue.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 111);
+            this.Controls.Add(this.lblValue);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.lblEnd);
+            this.Controls.Add(this.lblStart);
+            this.Controls.Add(this.dtpEnd);
+            this.Controls.Add(this.dtpStart);
+            this.Name = "Form1";
+            this.Text = "Dátumkülönbséginátor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpStart;
+        private System.Windows.Forms.DateTimePicker dtpEnd;
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.Label lblEnd;
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.Label lblValue;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA210104
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Dtp_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime start = dtpStart.Value, end = dtpEnd.Value;
+            TimeSpan timeSpan = end.Subtract(start);
+            if (end >= start) lblValue.Text = $"{timeSpan.Days} nap";
+            else lblValue.Text = "Érvénytelen formátum.";
+
+            //if (dtpEnd.Value < dtpStart.Value) MessageBox.Show("A végérték nagyobb, mint a kezdőérték!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dtpStart.Value = DateTime.Now;
+            dtpEnd.Value = DateTime.Now.AddMinutes(1);
+        }
+    }
+}
